83d0ec331fbbfee69ba4a371b6d51edc88a52044
Jemila <jemila.abulhawa@gmail.com>
2019-08-01 21:52:11 +0300

    create page query and use context

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,12 +1,33 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/prop-types */
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from './layout'
 
-const postLayout = () => (
-  <Layout>
-    <div>
-      <h1>Post Layout</h1>
-    </div>
-  </Layout>
-)
+const PostLayout = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { title } = data.markdownRemark.frontmatter
 
-export default postLayout
+  return (
+    <Layout>
+      <div>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+      }
+    }
+  }
+`
+
+export default PostLayout
