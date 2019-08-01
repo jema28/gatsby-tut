46cff308e479d696c3eca1435fb59291e4fde47d
Jemila <jemila.abulhawa@gmail.com>
2019-08-01 20:00:25 +0300

    create page in gatsby node

---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './layout'
+
+const postLayout = () => (
+  <Layout>
+    <div>
+      <h1>Post Layout</h1>
+    </div>
+  </Layout>
+)
+
+export default postLayout
